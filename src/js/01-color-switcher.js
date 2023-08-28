e28175bd55eba2eeaf2485ac064036a3882b4c1d
DimaRomanets <130175375+DimaRomanets@users.noreply.github.com>
2023-08-28 21:57:36 +0200

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+const CHANGE_COLOR = 1000;
+let idColor = null;
 
+const elements = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+elements.btnStop.disabled = true;
+
+// VAR 1
+elements.btnStart.addEventListener('click', () => {
+    elements.btnStart.disabled = true;
+    elements.btnStop.disabled = false;
+    idColor = setInterval(() => {
+    elements.body.style.backgroundColor = getRandomHexColor()
+}, CHANGE_COLOR)});
+
+
+elements.btnStop.addEventListener('click', () => {
+    elements.btnStart.disabled = false;
+    elements.btnStop.disabled = true;
+    
+    clearInterval(idColor);
+});
+
+
+// VAR 2
+
+
+// function onBtnStartChangeColor() {
+//     elements.btnStart.disabled = true;
+//     elements.btnStop.disabled = false;
+   
+
+//     idColor = setInterval(() => {
+//         elements.body.style.backgroundColor = getRandomHexColor()
+//     }, CHANGE_COLOR_DELAY);
+// }
+
+// function onBtnStopChangeColor() {
+//     elements.btnStart.disabled = false;
+//     elements.btnStop.disabled = true;
+    
+
+//     clearInterval(idColor);
+// }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
